b774d169ee96a8e2ab2341b7f24ac8ce1f2eccc9
张立鑫 <289030909@qq.com>
2016-01-08 17:03:41 +0800

    first commit

---
+++ TextFieldSelect/TextFieldSelect/TestFieldView.swift
@@ -0,0 +1,31 @@
+//
+//  TestFieldView.swift
+//  TextFieldSelect
+//
+//  Created by 张立鑫 on 16/1/8.
+//  Copyright © 2016年 zhang. All rights reserved.
+//
+
+import UIKit
+
+class TestFieldView: UITextField {
+
+    override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+}
---
+++ TextFieldSelect/TextFieldSelect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFieldSelect
+//
+//  Created by 张立鑫 on 16/1/8.
+//  Copyright © 2016年 zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
